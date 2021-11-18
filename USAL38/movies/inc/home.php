45e0c85d5c4e11f0c59fb08a5de1d25911ef5b38
MDevoldere <dev@devoldere.net>
2021-11-18 21:49:15 +0100

    1911P

---
+++ USAL38/movies/inc/home.php
@@ -0,0 +1,12 @@
+<section class="movies">
+    <h2>Tous les films</h2>
+    <article>
+        <h2>Film 1</h2>
+    </article>
+    <article>
+        <h2>Film 2</h2>
+    </article>
+    <article>
+        <h2>Film 3</h2>
+    </article>
+</section>
---
+++ USAL38/movies/index.php
@@ -0,0 +1,11 @@
+<?php 
+require 'inc/header.php';
+
+
+
+require 'inc/home.php';
+
+
+
+require 'inc/footer.php';
+?>
